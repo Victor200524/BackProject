d622e45bb9dcc1f75570a69d393f5e99299d8ef6
Victor200524 <victor.donaire@hotmail.com>
2024-11-09 13:31:22 -0300

    Atualizacao feita

--- Persistencia/produtoDAO.js
+++ Persistencia/produtoDAO.js
@@ -79,13 +79,13 @@ export default class ProdutoDAO {
         let sql = "";
         let parametros = [];
         if (isNaN(parseInt(termo))) { // produto p -> esse p é o apelido da tabela chamada produto
-            sql = `SELECT * FROM produto  
+            sql = `SELECT * FROM produto p 
                     INNER JOIN categoria ON prod_fk_codigo_cat = cat_codigo 
                     WHERE prod_descricao LIKE ?`;
             parametros = ['%' + termo + '%'];
         }
         else {
-            sql = `SELECT * FROM produto 
+            sql = `SELECT * FROM produto p
                     INNER JOIN categoria ON prod_fk_codigo_cat = cat_codigo
                     WHERE prod_codigo LIKE ?`;
             parametros = [termo];
